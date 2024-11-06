da7563ca5e020e36c7312d5a4b7a78ea16e1408a
leonardo829 <leo8292014@hotmail.com>
2024-11-06 19:55:41 -0300

    mas entidades

---
+++ Entidades/Detalle_Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Detalle_Pedido
+    {
+       public int ID_Detalle_pedido {  get; set; }
+        public int ID_Pedido { get; set; }
+        public int ID_Libro { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
