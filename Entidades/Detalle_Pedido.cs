0b2f8d886c2e729da6c609b77d398cfbad6080b8
Francisco Piñero <frannpinero@hotmail.com>
2024-11-06 20:40:42 -0300

    agaa

--- Entidades/Detalle_Pedido.cs
+++ Entidades/Detalle_Pedido.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entidades
+﻿namespace Entidades
 {
-    public class Detalle_Pedido
+	public class Detalle_Pedido
     {
        public int ID_Detalle_pedido {  get; set; }
         public int ID_Pedido { get; set; }
